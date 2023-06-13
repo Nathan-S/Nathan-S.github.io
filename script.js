2e50713d4030225e4569b89a26857e65c1982197
Nathanial Smith <58203695+Nathan-S@users.noreply.github.com>
2023-06-13 00:37:59 -0400

    switched from body to h1

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function moveBody(event) {
+function moveH1(event) {
     // Get the mouse position.
     var mouseX = event.clientX;
     var mouseY = event.clientY;
   
     // Get the title element.
-    var body = document.getElementById("body");
+    var h1 = document.getElementById("h1");
   
     // Calculate the new position of the title.
-    var newX = mouseX - body.offsetWidth / 2;
-    var newY = mouseY - body.offsetHeight / 2;
+    var newX = mouseX - h1.offsetWidth / 2;
+    var newY = mouseY - h1.offsetHeight / 2;
   
     // Move the title to the new position.
-    body.style.left = newX + "px";
-    body.style.top = newY + "px";
+    h1.style.left = newX + "px";
+    h1.style.top = newY + "px";
   }
